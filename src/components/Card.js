c8ae1d2392b36f5111cb264836af5ed4b884e80c
RobStein96 <49456466+RobStein96@users.noreply.github.com>
2019-10-14 11:24:01 -0700

    Fix cards layout

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Dimensions, Image, View, StyleSheet } from 'react-native';
+
+const{ width } = Dimensions.get('window');
+const cardWidth = (width - 108) / 4;
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: '#000',
+    borderWidth: 1,
+    margin: 10,
+  },
+
+  image: {
+    height: cardWidth,
+    width: cardWidth,
+  },
+});
+
+class Card extends React.Component {
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Image source = {this.props.source} style = {styles.image}/>
+      </View>
+    );
+  }
+}
+
+export default Card;
