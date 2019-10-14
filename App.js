b7cb8945657119d9beb060dd746f95219d88705d
RobStein96 <49456466+RobStein96@users.noreply.github.com>
2019-10-14 10:52:02 -0700

    Render cards on screen

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native'
+import Constants from 'expo-constants';
+import Cards from './src/components/Cards';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+console.log(Constants.statusBarHeight);
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
-});
+})
+
+export default function App() {
+  return (
+    <View style = {styles.container}>
+      <Cards/>
+    </View>
+  );
+}
